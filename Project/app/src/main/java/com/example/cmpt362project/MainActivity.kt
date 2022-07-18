28bb8974b3762f987f5b5d7e456ddd173a71b1f3
vgfang <vincent_fang@sfu.ca>
2022-07-18 12:49:17 -0700

    adding changes to main activity

--- Project/app/src/main/java/com/example/cmpt362project/MainActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/MainActivity.kt
@@ -14,10 +14,14 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
