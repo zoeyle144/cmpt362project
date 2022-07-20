82c82b3db324a9a7fca5df425b3415aa5e0d9eed
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-19 19:33:33 -0700

    Create dialog to switch between gallery/camera
    Doesn't do anything right now

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/UserProfileActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/UserProfileActivity.kt
@@ -1,14 +1,37 @@
 package com.example.cmpt362project.ui.settings
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cmpt362project.R
 
 class UserProfileActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
 
+    }
 
+    fun onClickChangePhoto(v: View) {
+        val builder = AlertDialog.Builder(this)
+        val dialogOptions = arrayOf("Open camera", "Select from gallery")
+
+        builder.setTitle("Upload profile picture")
+        builder.setItems(dialogOptions) {
+                _: DialogInterface, i: Int ->
+            if (dialogOptions[i] == "Open camera") {
+                println("Camera!")
+            }
+            else {
+                println("Gallery!")
+            }
+        }
+
+        val dialog = builder.create()
+        dialog.show()
     }
 }
