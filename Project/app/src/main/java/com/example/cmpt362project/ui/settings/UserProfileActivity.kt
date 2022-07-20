cb208e28541ff950277312a47aa2fca5eeab0b74
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-19 17:17:54 -0700

    Create activity for user profile

---
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cmpt362project.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cmpt362project.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
