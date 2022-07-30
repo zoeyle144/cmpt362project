2490b60911aebf60ac98d15d5fd150def0a077e8
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-30 01:27:32 -0700

    Create search user fragment
    Searching requires an activity, and I can't find a good way to directly connect the activity to the sidebar, so I'll have a fragment that sits in between the sidebar and the activity.

---
+++ Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
@@ -0,0 +1,20 @@
+package com.example.cmpt362project.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cmpt362project.R
+
+class SearchUserFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_search_user, container, false)
+
+        return view
+    }
+}
