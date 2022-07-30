a5608e45abf5aabcd14b45e6ba48ff2144c62516
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-30 00:26:10 -0700

    Create & declare searchable activity

---
+++ Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cmpt362project.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cmpt362project.R
+
+class SearchUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_user)
+    }
+}
