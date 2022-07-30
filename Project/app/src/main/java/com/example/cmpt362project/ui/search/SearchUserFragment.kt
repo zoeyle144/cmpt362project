a3cd98300663645133afdd432b9de9ffc52775cf
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-30 03:02:36 -0700

    Add string adapter to list view
    I'll turn it into a custom adapter soon

--- Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
@@ -2,10 +2,15 @@ package com.example.cmpt362project.ui.search
 
 import android.os.Bundle
 import android.view.*
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.example.cmpt362project.R
 
 class SearchUserFragment : Fragment() {
+
+    private val tempList = ArrayList<String>()
+    private lateinit var tempAdapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -13,6 +18,14 @@ class SearchUserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search_user, container, false)
+
+        for (i in 0..25) {
+            tempList.add(java.util.UUID.randomUUID().toString())
+        }
+
+        tempAdapter = activity?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, tempList) }!!
+        val myListView = view.findViewById<ListView>(R.id.search_user_search_results)
+        myListView.adapter = tempAdapter
 
         return view
     }
