697838a4a41adc7d1567f095dd9e5aa15e0bf9e8
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-30 02:37:10 -0700

    Add search view to fragment
    I tried to use the "Creating a Search Interface" document that Google made; it's too hard. I'll just do it my own way!

--- Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
@@ -2,14 +2,10 @@ package com.example.cmpt362project.ui.search
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Toolbar
-import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import com.example.cmpt362project.R
-import com.example.cmpt362project.databinding.FragmentSearchUserBinding
 
 class SearchUserFragment : Fragment() {
-    private lateinit var binding: FragmentSearchUserBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,13 +13,11 @@ class SearchUserFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search_user, container, false)
-        binding = FragmentSearchUserBinding.inflate(inflater, container, false)
 
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.searchUserFragmentToolbar.inflateMenu(R.menu.search_user_menu)
 
     }
 }
