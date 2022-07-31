909d93adb8bc557b00eef2c6bd02f7fb29f36c74
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-07-31 09:05:13 -0700

    Recycler view has its own adapter
    Apparently, to access SearchUserAdapter's filter() method, I have to make an instance of it in SearchUserFragment, then set recycler view's adapter to the instance.

--- Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/search/SearchUserFragment.kt
@@ -1,10 +1,8 @@
 package com.example.cmpt362project.ui.search
 
-import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
-import android.widget.ListView
 import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -12,23 +10,24 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cmpt362project.R
-import com.example.cmpt362project.database.User
 import com.example.cmpt362project.ui.search.placeholder.PlaceholderContent
-import com.example.cmpt362project.ui.settings.UserProfileActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class SearchUserFragment : Fragment() {
 
-    private val tempList = ArrayList<String>()
+//    private val tempList = ArrayList<String>()
     private lateinit var tempAdapter: ArrayAdapter<String>
     private var columnCount = 1
     private val tempItems = PlaceholderContent.ITEMS
+
+    private val listOfUsernames = ArrayList<String>()
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewAdapter: SearchUserAdapter
 
     private lateinit var database: DatabaseReference
 
@@ -39,15 +38,14 @@ class SearchUserFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search_user, container, false)
 
-        for (i in 0..25) {
-            tempList.add(java.util.UUID.randomUUID().toString())
-        }
+//        for (i in 0..25) {
+//            tempList.add(java.util.UUID.randomUUID().toString())
+//        }
 
 //        tempAdapter = activity?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, tempList) }!!
 //        val myListView = view.findViewById<ListView>(R.id.search_user_search_results)
 //        myListView.adapter = tempAdapter
 
-        val listOfNames = ArrayList<String>()
 
 
         database = Firebase.database.reference
@@ -56,8 +54,8 @@ class SearchUserFragment : Fragment() {
         allUsers.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for (i in snapshot.children) {
-                    val str = i.child("username").value
-                    println("str is $str")
+                    val name = i.child("username").value as String
+                    listOfUsernames.add(name)
                 }
             }
 
@@ -66,19 +64,21 @@ class SearchUserFragment : Fragment() {
 
 
 
-        val recyclerView = view.findViewById<RecyclerView>(R.id.search_user_search_results_recycler)
+        recyclerView = view.findViewById(R.id.search_user_search_results_recycler)
         recyclerView.layoutManager = when {
             columnCount <= 1 -> LinearLayoutManager(context)
             else -> GridLayoutManager(context, columnCount)
         }
-        recyclerView.adapter = SearchUserAdapter(PlaceholderContent.ITEMS)
+        recyclerViewAdapter = SearchUserAdapter(PlaceholderContent.ITEMS)
+        recyclerView.adapter = recyclerViewAdapter
 
-//        val searchView = view.findViewById<SearchView>(R.id.search_user_search_bar)
-//        val mySearchListener = SearchListener()
-//        searchView.setOnQueryTextListener(mySearchListener)
+        val searchView = view.findViewById<SearchView>(R.id.search_user_search_bar)
+        val mySearchListener = SearchListener()
+        searchView.setOnQueryTextListener(mySearchListener)
 
         return view
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
@@ -87,8 +87,10 @@ class SearchUserFragment : Fragment() {
     inner class SearchListener : SearchView.OnQueryTextListener {
         // https://www.geeksforgeeks.org/android-searchview-with-example/
         override fun onQueryTextSubmit(query: String?): Boolean {
-            if (tempList.contains(query)) {
-                tempAdapter.filter.filter(query)
+            if (listOfUsernames.contains(query)) {
+//                tempAdapter.filter.filter(query)
+                recyclerViewAdapter.filter.filter(query)
+
             } else {
                 Toast.makeText(activity, "Not found", Toast.LENGTH_LONG).show();
             }
@@ -96,7 +98,8 @@ class SearchUserFragment : Fragment() {
         }
 
         override fun onQueryTextChange(newText: String?): Boolean {
-            tempAdapter.filter.filter(newText);
+//            tempAdapter.filter.filter(newText);
+            recyclerViewAdapter.filter.filter(newText)
             return false;
         }
     }
