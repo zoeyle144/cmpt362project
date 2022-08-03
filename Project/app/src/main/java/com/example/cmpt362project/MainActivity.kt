8aebd177b5665c2a1cd17bb4c3d535dc3f07af20
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-02 21:34:56 -0700

    Show drawer icon in toolbar for chat fragment
    It didn't before (was back button) but now it shows the drawer

--- Project/app/src/main/java/com/example/cmpt362project/MainActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceCh
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_search, R.id.nav_settings, R.id.nav_groups), drawerLayout)
+                R.id.nav_home, R.id.nav_search, R.id.nav_settings, R.id.nav_chats, R.id.nav_groups), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
