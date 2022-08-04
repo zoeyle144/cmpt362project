8e3672e256842725a717bc83059845d0fbdd8d3c
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-04 05:01:51 -0700

    Rename root pref to acc sett pref, add logout
    1. root_preferences XML has been renamed to account_settings_pref XML to reflect its usage better.
    2. I added logout functionality to the account settings activity. It works.

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/SettingsAccountFragment.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/SettingsAccountFragment.kt
@@ -1,12 +1,24 @@
 package com.example.cmpt362project.ui.settings.account
 
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.cmpt362project.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SettingsAccountFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        setPreferencesFromResource(R.xml.account_settings_pref, rootKey)
+
+        val auth = Firebase.auth
+        val logoutBtn = findPreference<Preference>("logout")
+
+        logoutBtn!!.setOnPreferenceClickListener {
+            auth.signOut()
+            requireActivity().finishAffinity()
+            true
+        }
     }
 }
