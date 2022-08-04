a9ffde6a477507862bc470e32657e6ea52c5c915
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-04 04:38:39 -0700

    Create new activity that links to new set. frag
    The general settings frag has an entry "Edit your account" that opens up a new activity, SettingsAccountActivity.
    This activity has a FragmentContainerView that shows the SettingsAccountFragment.
    The SettingsAccountFragment is a PreferenceFragmentCompat that will later show the user options to edit their password, email, logout, etc.

---
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/SettingsAccountActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cmpt362project.ui.settings.account
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cmpt362project.R
+
+class SettingsAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_account)
+
+
+    }
+}
