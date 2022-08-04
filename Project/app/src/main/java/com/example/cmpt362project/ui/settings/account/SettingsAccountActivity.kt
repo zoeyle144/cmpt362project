88fcfd682913ade88649bc18c1462deeeaf42480
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-04 04:51:46 -0700

    Give SettingsAccountActivity toolbar
    The toolbar has a title and an X button. The X button doesn't do anything though as I need to make a menu XML and do onOptionsItemSelected!

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/SettingsAccountActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/SettingsAccountActivity.kt
@@ -2,6 +2,7 @@ package com.example.cmpt362project.ui.settings.account
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.cmpt362project.R
 
 class SettingsAccountActivity : AppCompatActivity() {
@@ -9,6 +10,8 @@ class SettingsAccountActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings_account)
 
-
+        val toolbar = findViewById<Toolbar>(R.id.settings_account_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
