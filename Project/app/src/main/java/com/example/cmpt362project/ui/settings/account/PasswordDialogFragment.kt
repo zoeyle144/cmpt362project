cffb2db07d73ceacde8a1e25825ae661cba968b8
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-04 20:43:32 -0700

    Changing passwords works
    I might make the app log you out after success.

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/PasswordDialogFragment.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/account/PasswordDialogFragment.kt
@@ -4,17 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.cmpt362project.R
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class PasswordDialogFragment : DialogFragment() {
+
+    private var user: FirebaseUser? = null
+    private lateinit var viewModel: EmailDialogFragmentViewModel
 
     private lateinit var currentPasswordView: TextInputLayout
     private lateinit var newPasswordView: TextInputLayout
     private lateinit var reNewPasswordView: TextInputLayout
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,9 +37,16 @@ class PasswordDialogFragment : DialogFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.account_settings_password_dialog, container, false)
 
+        user = Firebase.auth.currentUser
+        viewModel = ViewModelProvider(requireActivity())[EmailDialogFragmentViewModel::class.java]
+
         currentPasswordView = view.findViewById(R.id.enter_current_password_field)
         newPasswordView = view.findViewById(R.id.enter_new_password_field)
         reNewPasswordView = view.findViewById(R.id.reenter_new_password_field)
+
+        currentPasswordView.editText!!.addTextChangedListener { currentPasswordView.error = null }
+        newPasswordView.editText!!.addTextChangedListener { newPasswordView.error = null }
+        reNewPasswordView.editText!!.addTextChangedListener { reNewPasswordView.error = null }
 
         val toolbar = view.findViewById<Toolbar>(R.id.settings_account_toolbar)
         toolbar.inflateMenu(R.menu.account_settings_toolbar)
@@ -38,9 +54,10 @@ class PasswordDialogFragment : DialogFragment() {
         toolbar.setOnMenuItemClickListener {
             when(it.itemId) {
                 R.id.profile_toolbar_save -> {
+                    val currentPass = currentPasswordView.editText!!.text.toString()
                     val newPass = newPasswordView.editText!!.text.toString()
-                    val reNewPass = newPasswordView.editText!!.text.toString()
-                    setNewPassword(newPass, reNewPass)
+                    val reNewPass = reNewPasswordView.editText!!.text.toString()
+                    setNewPassword(currentPass, newPass, reNewPass)
                     true
                 }
                 else -> true
@@ -50,7 +67,26 @@ class PasswordDialogFragment : DialogFragment() {
         return view
     }
 
-    private fun setNewPassword(newPassword: String, reNewPassword: String) {
-
+    private fun setNewPassword(currPass: String, newPass: String, reNewPass: String) {
+        viewModel.reAuthenticate(user, currPass).observe(viewLifecycleOwner) { waitBoolean ->
+            when(waitBoolean) {
+                WaitBoolean.FALSE -> currentPasswordView.error = "Current password is incorrect."
+                WaitBoolean.TRUE -> {
+                    if (user != null) {
+                        if (newPass.isEmpty()) newPasswordView.error = "Enter new password field cannot empty."
+                        else if (newPass != reNewPass) reNewPasswordView.error = "Does not match the new password field."
+                        else {
+                            user!!.updatePassword(newPass).addOnSuccessListener {
+                                Toast.makeText(context, "Successfully changed password.", Toast.LENGTH_SHORT).show()
+                                dismiss()
+                            }.addOnFailureListener {
+                                newPasswordView.error = (it as FirebaseAuthWeakPasswordException).reason + "."
+                            }
+                        }
+                    }
+                }
+                else -> {}
+            }
+        }
     }
 }
