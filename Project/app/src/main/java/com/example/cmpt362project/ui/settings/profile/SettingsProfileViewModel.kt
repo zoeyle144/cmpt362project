4bd0645679b39b5508c16a29dc5008d2b70e268e
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-05 22:01:55 -0700

    Create SingleLiveEvent class
    I'll move SettingsProfileActivity stuff to the ViewModel. I need this SLE class to be able to "send" Toast messages from it.

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/profile/SettingsProfileViewModel.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/profile/SettingsProfileViewModel.kt
@@ -1,21 +1,23 @@
 package com.example.cmpt362project.ui.settings.profile
 
 import android.graphics.Bitmap
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cmpt362project.ui.settings.SingleLiveEvent
 
 class SettingsProfileViewModel : ViewModel() {
-    val profilePicture = MutableLiveData<Bitmap>()
 
-    //private lateinit var tempImage: Bitmap
+    val profilePicture = MutableLiveData<Bitmap>()
     private var imageSet = false
 
+    private val _toastMessage = MutableLiveData<SingleLiveEvent<String>>()
+    val toastMessage : LiveData<SingleLiveEvent<String>>
+        get() = _toastMessage
+
     fun getImage() : Bitmap? {
-        return if(imageSet) {
-            profilePicture.value
-        } else {
-            null
-        }
+        return if(imageSet) profilePicture.value
+        else null
     }
 
     fun setImage(bitmap: Bitmap) {
