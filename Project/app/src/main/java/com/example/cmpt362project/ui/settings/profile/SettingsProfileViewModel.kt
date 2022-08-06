8bc6a2397a86178673fbe597767e593143dd9dea
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-05 22:50:54 -0700

    Moving upload data/image to VM, part 1/?
    Need to make a view model factory class so I can pass in SharedPref.

--- Project/app/src/main/java/com/example/cmpt362project/ui/settings/profile/SettingsProfileViewModel.kt
+++ Project/app/src/main/java/com/example/cmpt362project/ui/settings/profile/SettingsProfileViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.cmpt362project.ui.settings.profile
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.cmpt362project.R
 import com.example.cmpt362project.ui.settings.SingleLiveEvent
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -12,6 +15,9 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.util.*
 
 class SettingsProfileViewModel : ViewModel() {
 
@@ -64,6 +70,63 @@ class SettingsProfileViewModel : ViewModel() {
                 _aboutMeViewText.value = userData["aboutMe"] as String
                 _profilePicPath.value = userData["profilePic"] as String
             }
+        }
+    }
+
+    fun saveProfile(name: String, aboutMe: String) {
+        database.child("users").child(user.uid).child("name").setValue(name).addOnSuccessListener {
+            database.child("users").child(user.uid).child("aboutMe").setValue(aboutMe).addOnSuccessListener {
+                uploadImage()
+            }
+        }
+    }
+
+    private fun uploadImage() {
+        val printIdentifier = "SettingsProfileActivity uploadImage"
+        val storage = Firebase.storage.reference
+        val image = getImage() ?: return
+
+        val imageScaled = Bitmap.createScaledBitmap(image, 240, 240, true)
+        val stream = ByteArrayOutputStream()
+        imageScaled.compress(Bitmap.CompressFormat.JPEG, 75, stream)
+        val byteArray = stream.toByteArray()
+        stream.close()
+
+        // Get the old profile picture path so we can delete the image later (if upload success)
+        val userReference = database.child("users").child(user.uid).child("profilePic")
+        userReference.get().addOnSuccessListener { oldImgPath ->
+
+            // Write the new profile picture to Storage
+            val randomUUID = UUID.randomUUID().toString().replace("-", "")
+            val newImgPath = "profilePic/$randomUUID.jpg"
+            storage.child(newImgPath).putBytes(byteArray).addOnSuccessListener {
+
+                // Write the new profile picture path to the user's info
+                userReference.setValue(newImgPath).addOnSuccessListener {
+                    println("$printIdentifier: Uploaded $newImgPath to database")
+
+                    // Delete the old profile picture from Storage, tell sidebar to update PFP
+                    // Do not delete the old PFP if it's the default one
+                    val pathToDelete = oldImgPath.value as String
+                    if (pathToDelete != getString(R.string.default_pfp_path)) {
+                        val oldImgRef = storage.child(oldImgPath.value as String)
+                        oldImgRef.delete().addOnSuccessListener {
+                            println("$printIdentifier: Deleted ${oldImgPath.value} from database")
+                            updateProfilePicSharedPref()
+                        }
+                    } else {
+                        updateProfilePicSharedPref()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun updateProfilePicSharedPref() {
+        val sharedPref = this.getSharedPreferences(SettingsProfileActivity.SHARED_PREF, Context.MODE_PRIVATE)
+        with(sharedPref.edit()) {
+            putBoolean(com.example.cmpt362project.ui.settings.profile.SettingsProfileActivity.KEY_PROFILE_PIC_RECENTLY_CHANGED, true)
+            apply()
         }
     }
 }
