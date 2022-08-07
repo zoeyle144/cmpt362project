33ef4cc79a677bc6b2b62ef3e619c4450a84f770
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-07 12:54:16 -0700

    LoginPageViewModel works and returns errors
    I tested login, email/username empty errors, and wrong credential errors. They all work, so the VM is successful.

--- Project/app/src/main/java/com/example/cmpt362project/login/LoginPageActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/login/LoginPageActivity.kt
@@ -7,8 +7,11 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
 import com.example.cmpt362project.MainActivity
 import com.example.cmpt362project.R
+import com.example.cmpt362project.login.LoginPageViewModel.LoginPageErrors.*
+import com.example.cmpt362project.login.LoginPageViewModel.LoginPageVMState.*
 import com.example.cmpt362project.utility.FieldsLayoutUtility
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
@@ -18,15 +21,19 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginPageActivity : AppCompatActivity() {
-    private var database: DatabaseReference = Firebase.database.reference
-    private var auth: FirebaseAuth = Firebase.auth
+//    private var database: DatabaseReference = Firebase.database.reference
+//    private var auth: FirebaseAuth = Firebase.auth
 
     private lateinit var emailView: TextInputLayout
     private lateinit var passwordView: TextInputLayout
 
+    private lateinit var viewModel: LoginPageViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.temp2)
+
+        viewModel = ViewModelProvider(this)[LoginPageViewModel::class.java]
 
         emailView = findViewById(R.id.login_email)
         passwordView = findViewById(R.id.login_password)
@@ -42,47 +49,88 @@ class LoginPageActivity : AppCompatActivity() {
             val email = emailView.editText!!.text.toString()
             val password = passwordView.editText!!.text.toString()
 
-            login(email, password)
+            login2(email, password)
         }
     }
 
-    private fun login(email: String, password: String) {
-        var checkFields = true
-        if (email.isEmpty()) {
-            emailView.error = "E-mail field cannot be empty."
-            checkFields = false
+    private fun login2(email: String, password: String) {
+        viewModel.login(email, password).observe(this) {
+            when (it) {
+                SUCCESS -> startMainActivity()
+                FAILURE -> handleLoginErrors()
+                else -> {}
+            }
         }
-        if (password.isEmpty()) {
-            passwordView.error = "Password field cannot be empty."
-            checkFields = false
-        }
-        if (!checkFields) {
-            return
-        } else {
-            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+    }
 
-                val user = auth.currentUser
-                database.child("users").child(user!!.uid).get().addOnSuccessListener {
-                    if (it.value == null) {
-                        Toast.makeText(baseContext, "Error: user doesn't have ID in the Realtime Database", Toast.LENGTH_SHORT).show()
-                    } else {
-                        val userData = it.value as Map<*, *>
+//    private fun login(email: String, password: String) {
+//        var checkFields = true
+//        if (email.isEmpty()) {
+//            emailView.error = "E-mail field cannot be empty."
+//            checkFields = false
+//        }
+//        if (password.isEmpty()) {
+//            passwordView.error = "Password field cannot be empty."
+//            checkFields = false
+//        }
+//        if (!checkFields) {
+//            return
+//        } else {
+//            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+//
+//                val user = auth.currentUser
+//                database.child("users").child(user!!.uid).get().addOnSuccessListener {
+//                    if (it.value == null) {
+//                        Toast.makeText(baseContext, "Error: user doesn't have ID in the Realtime Database", Toast.LENGTH_SHORT).show()
+//                    } else {
+//                        val userData = it.value as Map<*, *>
+//
+//                        Toast.makeText(this, "Logged in as: ${userData["username"]}", Toast.LENGTH_SHORT).show()
+//
+//                        // move on to new activity
+//                        val bundle = Bundle()
+//                        bundle.putString("username", userData["username"].toString())
+//                        bundle.putString("email", userData["email"].toString())
+//                        val intent = Intent(this, MainActivity::class.java)
+//                        intent.putExtras(bundle)
+//                        startActivity(intent)
+//                    }
+//                }
+//            }.addOnFailureListener {
+//                emailView.error = "E-mail may be incorrect."
+//                passwordView.error = "Password may be incorrect."
+//            }
+//        }
+//    }
 
-                        Toast.makeText(this, "Logged in as: ${userData["username"]}", Toast.LENGTH_SHORT).show()
+    private fun startMainActivity() {
+        val bundle = Bundle()
+        val data = viewModel.returnVal
 
-                        // move on to new activity
-                        val bundle = Bundle()
-                        bundle.putString("username", userData["username"].toString())
-                        bundle.putString("email", userData["email"].toString())
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtras(bundle)
-                        startActivity(intent)
+        Toast.makeText(this, "Logged in as: ${data.first}", Toast.LENGTH_SHORT).show()
+        bundle.putString("username", data.first)
+        bundle.putString("email", data.second)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
+    }
+
+    private fun handleLoginErrors() {
+        val errors = viewModel.errorList.value
+        if (errors != null) {
+            for (e in errors) {
+                when(e) {
+                    EMAIL_EMPTY -> emailView.error = "E-mail field cannot be empty."
+                    PASSWORD_EMPTY -> passwordView.error = "Password field cannot be empty."
+                    NO_USER_ID -> Toast.makeText(baseContext,
+                        "Error: user doesn't have ID in the Realtime Database", Toast.LENGTH_SHORT).show()
+                    WRONG_CREDENTIALS -> {
+                        emailView.error = "E-mail may be incorrect."
+                        passwordView.error = "Password may be incorrect."
                     }
                 }
-            }.addOnFailureListener {
-                emailView.error = "E-mail may be incorrect."
-                passwordView.error = "Password may be incorrect."
             }
+            viewModel.clearErrorList()
         }
     }
 
