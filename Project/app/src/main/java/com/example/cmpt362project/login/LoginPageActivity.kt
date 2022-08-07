bb726ded516329958934a3019f9bbbf9a56ca621
unectomy <78575769+kormithozine@users.noreply.github.com>
2022-08-07 12:20:09 -0700

    New UI works for LoginPageActivity
    You can log in with the new UI. The only errors that are shown are email empty, password empty, and email and/or password incorrect.

--- Project/app/src/main/java/com/example/cmpt362project/login/LoginPageActivity.kt
+++ Project/app/src/main/java/com/example/cmpt362project/login/LoginPageActivity.kt
@@ -1,15 +1,12 @@
 package com.example.cmpt362project.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.cmpt362project.MainActivity
 import com.example.cmpt362project.R
@@ -21,16 +18,16 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
-class LoginPageActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var database: DatabaseReference
-    private lateinit var auth: FirebaseAuth
+class LoginPageActivity : AppCompatActivity() {
+    private var database: DatabaseReference = Firebase.database.reference
+    private var auth: FirebaseAuth = Firebase.auth
 
     private lateinit var emailView: TextInputLayout
     private lateinit var passwordView: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
+        setContentView(R.layout.temp2)
 
         emailView = findViewById(R.id.login_email)
         passwordView = findViewById(R.id.login_password)
@@ -41,76 +38,56 @@ class LoginPageActivity : AppCompatActivity(), View.OnClickListener {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val loginButton = findViewById<Button>(R.id.login_submit_btn)
-        loginButton.setOnClickListener(this)
-    }
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val email = emailView.editText!!.text.toString()
+            val password = passwordView.editText!!.text.toString()
 
-
-    // display error message;  if view is not null, set text color to error color
-    fun showError(errorMsg: String, view: EditText?) {
-        var er: TextView = findViewById<TextView>(R.id.error_message)
-        er.text = errorMsg
-        if (view != null) {
-            view.setTextColor(resources.getColor(R.color.error))
+            logIn(email, password)
         }
     }
 
-    override fun onClick(v: View) {
-        val email = findViewById<EditText>(R.id.login_email)
-        val password = findViewById<EditText>(R.id.login_password)
-
-        val emailTxt = email.text.toString()
-        val passwordTxt = password.text.toString()
-
-        if (emailTxt.isEmpty()) {
-            showError("Error: Username is Empty", email)
-            return
-        } else if (passwordTxt.isEmpty()) {
-            showError("Error: Password is Empty", password)
-            return
-        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailTxt).matches()) {
-            showError("Error: Invalid Email", email)
-            return
+    private fun logIn(email: String, password: String) {
+        var checkFields = true
+        if (email.isEmpty()) {
+            emailView.error = "E-mail field cannot be empty."
+            checkFields = false
         }
+        if (password.isEmpty()) {
+            passwordView.error = "Password field cannot be empty."
+            checkFields = false
+        }
+        if (!checkFields) {
+            return
+        } else {
+            auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
 
-        // back-end input validation
-        database = Firebase.database.reference
-        auth = Firebase.auth
+                val user = auth.currentUser
+                database.child("users").child(user!!.uid).get().addOnSuccessListener {
+                    if (it.value == null) {
+                        Toast.makeText(baseContext, "Error: user doesn't have ID in the Realtime Database", Toast.LENGTH_SHORT).show()
+                    } else {
+                        val userData = it.value as Map<*, *>
 
-        auth.signInWithEmailAndPassword(emailTxt, passwordTxt)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    // success
-                    database.child("users").child(user!!.uid).get().addOnSuccessListener {
-                        if (it.value == null) {
-                            // missing entire entry in database
-                            showError("Error: Missing Entry", null)
-                        } else {
-                            val userData = it.value as Map<*, *>
+                        Toast.makeText(this, "Logged in as: ${userData["username"]}", Toast.LENGTH_SHORT).show()
 
-                            Log.w("TAG", userData.toString())
-                            Toast.makeText(
-                                this, "Successfully logged in as: " +
-                                        "${userData["username"]}", Toast.LENGTH_SHORT
-                            ).show()
-                            // move on to new activity
-                            val bundle = Bundle()
-                            bundle.putString("username", userData["username"].toString())
-                            bundle.putString("email", userData["email"].toString())
-                            val intent = Intent(this, MainActivity::class.java)
-                            intent.putExtras(bundle)
-                            startActivity(intent)
-                        }
+                        // move on to new activity
+                        val bundle = Bundle()
+                        bundle.putString("username", userData["username"].toString())
+                        bundle.putString("email", userData["email"].toString())
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtras(bundle)
+                        startActivity(intent)
                     }
-                } else {
-                    // If sign in fails, display a message to the user.
-                    showError("Error: Username or password is incorrect.", null)
                 }
+            }.addOnFailureListener {
+                emailView.error = "E-mail may be incorrect."
+                passwordView.error = "Password may be incorrect."
             }
+        }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
         else -> {
             finish()
             true
